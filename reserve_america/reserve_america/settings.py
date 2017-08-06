b0bd8c308193f414b3555f6958391ed8a979e82c
shaoke <shaoke.xu@oracle.com>
2017-08-06 09:17:52 -0700

    remove unused files and add a middleware - user agents

--- reserve_america/reserve_america/settings.py
+++ reserve_america/reserve_america/settings.py
@@ -23,13 +23,17 @@ FEED_URI = 'parks.json'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+DOWNLOADER_MIDDLEWARES = {
+    "reserve_america.middlewares.UserAgentMiddleware": 401
+}
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
