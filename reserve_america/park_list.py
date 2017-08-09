dfc5397bd47fa21744734d370d3076cd264b6640
shaoke <shaoke.xu@oracle.com>
2017-08-08 23:51:04 -0700

    add parks

--- reserve_america/park_list.py
+++ reserve_america/park_list.py
@@ -2,5 +2,17 @@ park_list = [
     {
         "contractCode": "NRSO",
         "parkId": 74098
+    },
+    {
+        "contractCode": "NRSO",
+        "parkId": 70459
+    },
+    {
+        "contractCode": "NRSO",
+        "parkId": 70230
+    },
+    {
+        "contractCode": "NRSO",
+        "parkId": 70231
     }
 ]
